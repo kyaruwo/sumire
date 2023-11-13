3616e6b4a660aec8deef9531953943a640dc7d52
kyaruwo <75692623+kyaruwo@users.noreply.github.com>
2023-11-13 23:25:58 +0800

    Update schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -3,6 +3,6 @@ CREATE DATABASE sumire;
 use sumire;
 CREATE TABLE Notes (
     id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(69),
-    body TEXT
+    title VARCHAR(69) NOT NULL,
+    body TEXT NOT NULL
 );
