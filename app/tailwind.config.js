027d42471c265e3fe957d1b118ece9822681ae06
kyaruwo <75692623+kyaruwo@users.noreply.github.com>
2023-12-26 18:28:43 +0800

    tailwind setup

---
+++ app/tailwind.config.js
@@ -0,0 +1,12 @@
+/*
+cd app & tailwindcss\tailwindcss.exe -i tailwindcss\tailwind.css -o css\sumire.css --minify --watch
+*/
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["index.html", "html/*.html", "js/*.js"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
