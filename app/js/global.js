cd57a1e1b2bc451066167e1f91b50db4d413210a
kyaruwo <75692623+kyaruwo@users.noreply.github.com>
2024-01-02 20:51:58 +0800

    toast loader

--- app/js/global.js
+++ app/js/global.js
@@ -1,4 +1,8 @@
 (async function () {
+    if (location.host == "127.0.0.1:5500") {
+        return;
+    }
+
     const token = Cookies.get("token");
 
     if (token == undefined) {
@@ -30,6 +34,23 @@
         console.log(e);
         toast("Error", "An error occurred");
     }
+})();
+
+(function () {
+    const body = document.getElementById("body");
+
+    body.innerHTML =
+        `
+    <div
+        id="toast"
+        class="fixed right-0 hidden h-20 w-80 rounded-xl bg-white font-bold text-black max-lg:h-16 max-lg:w-60"
+    >
+        <div class="flex h-full flex-col items-center justify-center">
+            <p id="response_title" class="text-lg">Toast Response Title</p>
+            <p id="response_body" class="text-sm">Toast Response Body</p>
+        </div>
+    </div>
+        ` + body.innerHTML;
 })();
 
 async function toast(title, body) {
