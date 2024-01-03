034abead9f6199e76436bb7df0816e2b2f6b9578
kyaruwo <75692623+kyaruwo@users.noreply.github.com>
2024-01-03 09:06:59 +0800

    merge about to index

---
+++ app/js/index.js
@@ -0,0 +1,54 @@
+if (location.hash == "#about") {
+    show_about();
+} else {
+    show_sumire();
+}
+
+function show_sumire() {
+    location.hash = "sumire";
+    const main = document.getElementById("main");
+    main.innerHTML = `
+    <h class="text-8xl max-lg:text-7xl max-md:text-6xl max-sm:text-5xl"
+        >Write down Notes</h
+    >
+    `;
+
+    if (!Cookies.get("token")) {
+        main.innerHTML += `    
+        <a
+            class="mb-2 mt-12 rounded-full bg-green-600 px-24 py-3 text-2xl max-md:px-20 max-md:text-xl"
+            href="/app/html/auth.html#register"
+        >
+            Register
+        </a>
+        <p class="max-md:text-sm">
+            Already have an account?
+            <a class="text-blue-400" href="/app/html/auth.html#login"
+                >Login</a
+            >
+        </p>
+        `;
+    } else {
+        main.innerHTML += `    
+        <a
+            class="mb-2 mt-12 rounded-full bg-green-600 px-24 py-3 text-2xl max-md:px-20 max-md:text-xl"
+            href="/app/html/main.html"
+        >
+            Write
+        </a>
+        `;
+    }
+}
+
+function show_about() {
+    location.hash = "about";
+    document.getElementById("main").innerHTML = `
+    <a href="https://kyaruwo.github.io/" target="_blank">
+        <img
+            src="/app/assets/tako.gif"
+            class="max-md:size-40 max-lg:size-52 lg:size-60 -mt-12"
+    /></a>
+    <h class="mt-2 text-4xl font-bold">kyaruwo</h>
+    <p class="text-xl">Developer</p>
+    `;
+}
