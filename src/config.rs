36c302166c909532b556e9a800e01259ec8be719
kyaruwo <75692623+kyaruwo@users.noreply.github.com>
2024-01-06 19:49:58 +0800

    update config

--- src/config.rs
+++ src/config.rs
@@ -1,14 +1,21 @@
+use tokio::net::TcpListener;
+
 pub struct Config {
+    pub tcp_listener: TcpListener,
     pub address: String,
     pub database_url: String,
     pub aes_key: String,
 }
 
-pub fn load() -> Config {
+pub async fn load() -> Config {
     dotenvy::dotenv().expect("\".env\" file is missing");
 
     let address: String =
         dotenvy::var("ADDRESS").expect("\"ADDRESS\"  is missing from \".env\" file");
+
+    let tcp_listener: TcpListener = TcpListener::bind(&address)
+        .await
+        .expect("\"ADDRESS\" is invalid or inuse");
 
     let database_url: String =
         dotenvy::var("DATABASE_URL").expect("\"DATABASE_URL\" is missing from \".env\" file");
@@ -18,6 +25,7 @@ pub fn load() -> Config {
 
     Config {
         address,
+        tcp_listener,
         database_url,
         aes_key,
     }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use axum::{serve, Extension, Router};
 use sqlx::{mysql::MySqlPoolOptions, MySql, Pool};
-use tokio::net::TcpListener;
 
 mod api;
 mod app;
@@ -8,11 +7,7 @@ mod config;
 
 #[tokio::main]
 async fn main() {
-    let config: config::Config = config::load();
-
-    let address: TcpListener = TcpListener::bind(&config.address)
-        .await
-        .expect("\"ADDRESS\" is invalid or inuse");
+    let config: config::Config = config::load().await;
 
     let db_pool: Pool<MySql> = match MySqlPoolOptions::new()
         .max_connections(4)
@@ -33,7 +28,7 @@ async fn main() {
     println!(" backend @ http://{}/api", config.address);
     println!("frontend @ http://{}/app", config.address);
 
-    serve(address, app.into_make_service())
+    serve(config.tcp_listener, app.into_make_service())
         .await
         .expect("\nsumire died");
 }
