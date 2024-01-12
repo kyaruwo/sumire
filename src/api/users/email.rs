4e8a885c25180b5308a5c68ad9cfe6e25e8a8022
kyaruwo <75692623+kyaruwo@users.noreply.github.com>
2024-01-12 11:31:32 +0800

    Update email.rs

--- src/api/users/email.rs
+++ src/api/users/email.rs
@@ -1,10 +1,24 @@
-use axum::{routing::put, Router};
+use axum::{
+    routing::{post, put},
+    Router,
+};
 use sqlx::{Pool, Postgres};
 
 pub fn routes() -> Router<Pool<Postgres>> {
-    Router::new().route("/email", put(update_email))
+    Router::new()
+        .route("/verify_email", put(verify_email))
+        .route("/change_email", post(change_email))
+        .route("/new_email", put(new_email))
 }
 
-async fn update_email() {
+async fn verify_email() {
+    todo!()
+}
+
+async fn change_email() {
+    todo!()
+}
+
+async fn new_email() {
     todo!()
 }
